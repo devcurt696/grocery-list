86fd1cc347abfcb510c15a82f6e3ed04a91518f9
curtcurt69 <cecrouse96@yahoo.com>
2023-12-16 18:11:02 -0500

    Grocery list App with Nextjs, Prisma, trpc and Postgres

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,34 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@fontsource/poppins';
+import type { AppType } from 'next/app';
+import { ServerRouter } from '@/server/router';
+import { createTRPCNext } from '@trpc/next';
+import { httpBatchLink } from '@trpc/client';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function getBaseUrl() {
+  if (typeof window === 'undefined') {
+    return process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : 'http://localhost:3000/api/trpc';
+  }
+
+  return '/api/trpc';
 }
+
+const { withTRPC } = createTRPCNext<ServerRouter>({
+  config({ctx}) {
+    const links = [
+      httpBatchLink({
+        url: getBaseUrl(),
+      }),
+    ];
+    return { links };
+  },
+  ssr: true,
+});
+
+const App: AppType = ({ Component, pageProps}) => {
+  return <Component {...pageProps} />;
+};
+
+export default withTRPC(App);
